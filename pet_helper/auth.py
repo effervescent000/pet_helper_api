5ee4260c9567c63c0687fb7f1e0463b46cae52a2
effervescent000 <kislio01@gmail.com>
2022-04-04 11:53:22 -0400

    a couple of auth endpoints

---
+++ pet_helper/auth.py
@@ -0,0 +1,77 @@
+from flask import Blueprint, request, jsonify, current_app
+from datetime import datetime, timedelta, timezone
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt_identity,
+    set_access_cookies,
+    get_jwt,
+    unset_jwt_cookies,
+    jwt_required,
+    current_user,
+)
+
+from .models import User
+from .schema import UserSchema
+from . import db, jwt
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+one_user_schema = UserSchema()
+multi_user_schema = UserSchema(many=True)
+
+# POST endpoints
+
+
+@bp.route("/signup", methods=["POST"])
+def signup():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    if User.query.filter_by(username=username).first() == None:
+        new_user = User(username=username, password=User.hash_password(password))
+        db.session.add(new_user)
+        db.session.commit()
+        response = jsonify(one_user_schema.dump(new_user))
+        access_token = create_access_token(identity=username)
+        set_access_cookies(response, access_token)
+        return response
+    return jsonify("User already exists"), 401
+
+
+@bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get("username")
+    password = data.get("password")
+    user_query = User.query.filter_by(username=username).first()
+    if user_query == None:
+        return jsonify("Invalid username/password"), 401
+    if not user_query.check_password(password):
+        return jsonify("Invalid username/password"), 401
+    response = jsonify(one_user_schema.dump(user_query))
+    access_token = create_access_token(identity=username)
+    set_access_cookies(response, access_token)
+    return response
+
+
+# utils
+
+
+@current_app.after_request
+def refresh_expiring_jwts(response):
+    try:
+        exp_timestamp = get_jwt()["exp"]
+        now = datetime.now(timezone.utc)
+        target_timestamp = datetime.timestamp(now + timedelta(minutes=30))
+        if target_timestamp > exp_timestamp:
+            access_token = create_access_token(identity=get_jwt_identity())
+            set_access_cookies(response, access_token)
+        return response
+    except (RuntimeError, KeyError):
+        # Case where there is not a valid JWT. Just return the original respone
+        return response
+
+
+@jwt.user_lookup_loader
+def user_lookup_callback(_jwt_header, jwt_data):
+    identity = jwt_data["sub"]
+    return User.query.filter_by(username=identity).first()
